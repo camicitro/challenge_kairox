96f21b7207a1390badb6f4cb250f8d63c867efdd
camicitro <camilacitro1106@gmail.com>
2024-08-22 20:48:57 -0300

    Modelo afiliado

--- src/models/AffiliateModel.ts
+++ src/models/AffiliateModel.ts
@@ -1,3 +1,25 @@
-import { Model, DataType } from "sequelize";
+import { Model, DataType, DataTypes } from "sequelize";
+import { sequelize } from "../config/database/data.source";
 
+export class Affiliate extends Model{
+    affiliateName!: string;
+    affiliateEmail!: string;
+    affiliateDni!: number;
+    affiliateNumber!: number;
+    affiliationEndDate!: Date;
+}
 
+Affiliate.init({
+        affiliateName: { type: DataTypes.STRING },
+        affiliateEmail: { type: DataTypes.STRING },
+        affiliateDni: { type: DataTypes.INTEGER },
+        affiliateNumber: { type: DataTypes.INTEGER },
+        affiliationEndDate: { type: DataTypes.DATE }
+    }, {
+        sequelize,
+        modelName: 'Affiliate',
+        tableName: 'affiliates',
+        timestamps: false
+});
+
+export default Affiliate
