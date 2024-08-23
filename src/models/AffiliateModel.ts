5017d1f5df4d93f23e04a9621ebfe4d31574887c
camicitro <camilacitro1106@gmail.com>
2024-08-22 23:26:55 -0300

    Correccion modelo afiliado

--- src/models/AffiliateModel.ts
+++ src/models/AffiliateModel.ts
@@ -1,25 +1,31 @@
 import { Model, DataType, DataTypes } from "sequelize";
 import { sequelize } from "../config/database/data.source";
 
-export class Affiliate extends Model{
-    affiliateName!: string;
-    affiliateEmail!: string;
-    affiliateDni!: number;
-    affiliateNumber!: number;
-    affiliationEndDate!: Date;
-}
-
-Affiliate.init({
-        affiliateName: { type: DataTypes.STRING },
-        affiliateEmail: { type: DataTypes.STRING },
-        affiliateDni: { type: DataTypes.INTEGER },
-        affiliateNumber: { type: DataTypes.INTEGER },
-        affiliationEndDate: { type: DataTypes.DATE }
+export const Affiliate = sequelize.define(
+    "Affiliate",
+    {
+        affiliateName: { 
+            type: DataTypes.STRING
+        },
+        affiliateEmail: {
+            type: DataTypes.STRING
+        },
+        affiliateDni: { 
+            type: DataTypes.INTEGER.UNSIGNED,
+            unique: true
+        },
+        affiliateNumber: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            unique: true
+        },
+        affiliationEndDate: {
+            type: DataTypes.DATE,
+            unique: true 
+        }
     }, {
-        sequelize,
-        modelName: 'Affiliate',
         tableName: 'affiliates',
         timestamps: false
 });
+
 
 export default Affiliate
