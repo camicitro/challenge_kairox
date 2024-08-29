629cc267ccd89b1dcb4c93e70dd6ff66d9dbf053
Agustina Linares <143526107+aguslinares@users.noreply.github.com>
2024-08-29 16:14:52 -0300

    ruta para eliminar 1afi eliminada

--- src/routes/AffiliateRoutes.ts
+++ src/routes/AffiliateRoutes.ts
@@ -13,8 +13,6 @@ const affiliateController = new AffiliateController(affiliateService);
 
 const affiliateRouter = Router();
 
-affiliateRouter.put('/affiliates/deactivate/:dni', (req, res) => affiliateController.deactivateAffiliate(req, res));
-
 affiliateRouter.put('/affiliates/deactivateAffiliates', (req, res) => affiliateController.processAffiliatesDeactivation(req, res));
 
 affiliateRouter.get('/affiliates/affiliatesInDebt', (req, res) => affiliateController.findAffiliatesInLongTermDebt(req, res));
